582a6d2305baa99a3a88be859ed289f5bc3cdad6
Jose Roberto Costa Junior <jose.junior@contmatic.com.br>
2019-09-03 12:53:37 -0300

    Projeto

--- projeto-api/src/main/java/br/com/contmatic/projeto/ProjetoApplication.java
+++ projeto-api/src/main/java/br/com/contmatic/projeto/ProjetoApplication.java
@@ -1,13 +1,13 @@
-package br.com.contmatic.springboot;
+package br.com.contmatic.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class ProjetoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+		SpringApplication.run(ProjetoApplication.class, args);
 	}
 
 }
